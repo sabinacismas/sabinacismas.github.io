90a90e5e3bea4065238d1ada80310b60becb9163
Sabina <sabinacismas02@gmail.com>
2025-03-16 17:10:01 -0400

    layout changes to index and gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'heading': ['Poppins', 'sans-serif'],
+        'body': ['Lora', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
